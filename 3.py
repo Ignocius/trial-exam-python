5cf6bf2a4412dc06535d06469f11121eb1d538bb
Szabo Rihcard <ignocius@gmail.com>
2016-11-29 19:12:20 +0100

    third task complete

--- 3.py
+++ 3.py
@@ -2,3 +2,22 @@
 # It should take its three sizes as constructor parameters (numbers)
 # It should have a method called `getSurface` that returns the cuboid's surface
 # It should have a method called `getVolume` that returns the cuboid's volume
+
+class Cuboid:
+    def __init__(self, a_side, b_side, c_side):
+        self.a_side = a_side
+        self.b_side = b_side
+        self.c_side = c_side
+
+    def getSurface(self):
+        #   TSA= 2(ab + bc +ca)
+        return 2*(self.a_side*self.b_side + self.b_side*self.c_side + self.c_side*self.a_side)
+
+    def getVolume(self):
+        # V = a * b* c
+        return self.a_side * self.b_side * self.c_side
+
+
+teglalap = Cuboid(10,5,4)
+print(teglalap.getSurface())
+print(teglalap.getVolume())
