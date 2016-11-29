a85b0d998b55512691dac9037393ee381d35b39a
Szabo Rihcard <ignocius@gmail.com>
2016-11-29 19:02:47 +0100

    first and second task complete

--- 1.py
+++ 1.py
@@ -1,3 +1,15 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
+
+def list_data_doubler(data_list):
+    if isinstance(data_list,list):
+        reference_list = []
+        for i in data_list:
+            reference_list.append(i*2)
+        return reference_list
+    else:
+        raise TypeError("Please give me a list for parameter!")
+
+lista = [1,2,3,4]
+print(list_data_doubler(lista))
--- 2.py
+++ 2.py
@@ -1,3 +1,18 @@
 # Create a function that takes a filename as string parameter,
 # and counts the occurances of the letter "a", and returns it as a number.
 # It should not break if the file does not exist, just return 0.
+
+def letter_counter_from_file(file_name):
+    try:
+        f = open(file_name, "r")
+        read = f.readlines()
+        a_counter = 0
+        for line in range(len(read)):
+            for word in read[line]:
+                if word == "a":
+                    a_counter += 1
+        print(a_counter)
+    except FileNotFoundError:
+        print(0)
+
+letter_counter_from_file("tesst.txt")
