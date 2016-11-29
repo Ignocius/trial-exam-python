cfd9a4a98a04326537a90332727d6b3a4f4e870e
Szabo Rihcard <ignocius@gmail.com>
2016-11-29 20:12:30 +0100

    5th with some issue

--- 5.py
+++ 5.py
@@ -1,11 +1,31 @@
 pirates = [
-  {'Name': 'Olaf', 'has_wooden_leg': False, 'gold': 12},        
-  {'Name': 'Uwe', 'has_wooden_leg': True, 'gold': 9},        
-  {'Name': 'Jack', 'has_wooden_leg': True, 'gold': 16},        
-  {'Name': 'Morgan', 'has_wooden_leg': False, 'gold': 17},        
-  {'Name': 'Hook', 'has_wooden_leg': True, 'gold': 20},        
+  {'Name': 'Olaf', 'has_wooden_leg': False, 'gold': 12},
+  {'Name': 'Uwe', 'has_wooden_leg': True, 'gold': 9},
+  {'Name': 'Jack', 'has_wooden_leg': True, 'gold': 16},
+  {'Name': 'Morgan', 'has_wooden_leg': False, 'gold': 17},
+  {'Name': 'Hook', 'has_wooden_leg': True, 'gold': 20},
 ]
 
 # Write a function that takes any list that contains pirates as in the example,
 # And returns a list of names containing the pirates that has wooden leg and
 # more than 15 gold
+
+def pirate_sorter_woodenleg_gold(pirates_input):
+    pirates_with_gold = []
+    for i in pirates_input:
+        # print(i)
+        if i['gold'] > 15 and i['has_wooden_leg'] == True:
+            print(i['Name'])
+            return pirates_with_gold.append(i['Name'])
+
+
+piratess = [
+  {'Name': 'Olaf', 'has_wooden_leg': False, 'gold': 12},
+  {'Name': 'Uwe', 'has_wooden_leg': True, 'gold': 9},
+  {'Name': 'Jack', 'has_wooden_leg': True, 'gold': 16},
+  {'Name': 'Morgan', 'has_wooden_leg': False, 'gold': 17},
+  {'Name': 'Hook', 'has_wooden_leg': True, 'gold': 20},
+  {'Name': 'Rook', 'has_wooden_leg': True, 'gold': 20},
+  {'Name': 'Took', 'has_wooden_leg': True, 'gold': 27},
+]
+print(pirate_sorter_woodenleg_gold(piratess))
